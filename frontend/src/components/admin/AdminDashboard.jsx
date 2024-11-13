023cd72ed2fcfeaf354ce733144bac18c724556e
Paula Giraldo 2022411 <maria.paula.giraldo@correounivalle.edu.co>
2024-11-12 22:45:02 -0500

    gestionbanners admin

--- frontend/src/components/admin/AdminDashboard.jsx
+++ frontend/src/components/admin/AdminDashboard.jsx
@@ -15,8 +15,9 @@ import {
   CardContent,
 } from "@mui/material";
 import Grid from '@mui/material/Grid';
-import { Menu as MenuIcon, Notifications, Home, People, Inventory, ShoppingCart, Logout, Assignment} from "@mui/icons-material";
+import { Menu as MenuIcon, Notifications, Home, People, Inventory, ShoppingCart, Logout, Assignment, CoPresent} from "@mui/icons-material";
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import CoPresentIcon from '@mui/icons-material/CoPresent';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import Cookies from 'js-cookie';
 import axios from "axios";
@@ -25,6 +26,7 @@ import Informes from './Informes';
 import Orders from './Orders';
 import Users from './Users';
 import GestionSolicitudes from './GestionSolicitudes';
+import VistaUser from './GestionVistaUser';
 
 export default function AdminDashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -239,6 +241,8 @@ export default function AdminDashboard() {
         return <Users />;
       case 'solicitudes':
         return <GestionSolicitudes />;
+      case 'vistaUser':
+        return <VistaUser />;
       default:
         return <div>Bienvenido al Panel de Administración</div>;
     }
@@ -297,6 +301,10 @@ export default function AdminDashboard() {
             <ListItem button onClick={() => setSelectedSection('solicitudes')}>
               <ListItemIcon><ModeCommentIcon /></ListItemIcon>
               {sidebarOpen && <ListItemText primary="Gestión Solicitudes" />}
+            </ListItem>
+            <ListItem button onClick={() => setSelectedSection('vistaUser')}>
+              <ListItemIcon><CoPresentIcon /></ListItemIcon>
+              {sidebarOpen && <ListItemText primary="Gestión vista Usuario" />}
             </ListItem>
             <ListItem button onClick={handleLogout}>
               <ListItemIcon><Logout /></ListItemIcon>
